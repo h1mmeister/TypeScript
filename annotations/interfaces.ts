9ee55b0c7de19f0273f79fd878aa6ec7cba71335
h1m <himanshu.yadav643@gmail.com>
2021-03-30 06:47:50 +0530

    understanding interfaces

---
+++ annotations/interfaces.ts
@@ -0,0 +1,29 @@
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+  summary(): string;
+}
+
+const oldCivic = {
+  name: "civic",
+  year: 2000,
+  broken: true,
+  summary(): string {
+    return `Name is ${this.name}`;
+  },
+};
+
+// const printVehicle = (vehicle: {
+//   name: string;
+//   year: number;
+//   broken: boolean;
+// }): void => {
+//   console.log(`Name: ${vehicle.name}`);
+// };
+
+const printVehicle = (vehicle: Vehicle): void => {
+  console.log(`Name is ${vehicle.name}`);
+};
+
+printVehicle(oldCivic); // the only problem is the lenght of type annotation that we are doing
