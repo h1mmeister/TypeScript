ef4348d1342d2af8027f68db393c5397eafbfdfc
h1m <himanshu.yadav643@gmail.com>
2021-04-09 02:55:27 +0530

    created company class and embedded maps

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hi, there!");
+import { User } from "./User";
+import { Comapny } from "./Company";
+
+const user = new User();
+const company = new Comapny();
+console.log(user);
+console.log(company);
