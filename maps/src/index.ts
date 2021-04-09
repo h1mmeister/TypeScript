1e8bfb5e42991dbf7b0640900bf212ae05ac32af
h1m <himanshu.yadav643@gmail.com>
2021-04-09 17:36:45 +0530

    showing map to the screen

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,7 +1,15 @@
 import { User } from "./User";
 import { Comapny } from "./Company";
 
-const user = new User();
-const company = new Comapny();
-console.log(user);
-console.log(company);
+// const user = new User();
+// const company = new Comapny();
+// console.log(user);
+// console.log(company);
+
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
