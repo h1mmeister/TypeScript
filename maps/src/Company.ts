3fef06e064c22eeb3eeda93f1762f6c676dbcc81
h1m <himanshu.yadav643@gmail.com>
2021-04-23 02:11:11 +0530

    completed maps widget, implemented interfaces

--- maps/src/Company.ts
+++ maps/src/Company.ts
@@ -1,6 +1,7 @@
 import faker, { fake } from "faker";
+import { Mappable } from "./CustomMap";
 
-export class Company {
+export class Company implements Mappable {
   companyName: string;
   catchPhrase: string;
   location: {
@@ -15,5 +16,9 @@ export class Company {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude()),
     };
+  }
+
+  markerContent(): string {
+    return `<div> <h3>Company Name: ${this.companyName}</h3> </div>`;
   }
 }
