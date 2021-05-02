f4bf0b50cc739a6aa25c54cf919977c7c9f6be3e
h1m <himanshu.yadav643@gmail.com>
2021-05-02 14:23:36 +0530

    refactoring bad code - adding NumberCollections.ts

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,35 +1,3 @@
-class Sorter {
-  //   collection: number[];
-
-  //   constructor(collection: number[]) {
-  //     this.collection = collection;
-  //   }
-
-  constructor(public collection: number[] | string) {}
-
-  sort(): void {
-    // const length = this.collection.length;
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection instanceof Array) {
-          // Type Guard for Array
-          if (this.collection[j] > this.collection[j + 1]) {
-            const temp = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = temp;
-          }
-        }
-
-        if (typeof this.collection === "string") {
-          // tType Guard for String
-        }
-      }
-    }
-  }
-}
-
 const sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
 console.log(sorter.collection);
