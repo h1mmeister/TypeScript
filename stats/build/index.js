11a64c581df305645a780ec89321739f72dd13eb
h1m <himanshu.yadav643@gmail.com>
2021-05-16 16:53:15 +0530

    moving grom bad approach to good using typescript

--- stats/build/index.js
+++ stats/build/index.js
@@ -13,13 +13,18 @@ var matches = fs_1.default
     return row.split(",");
 });
 // console.log(matches);
+var MatchResult = {
+    HomeWin: "H",
+    AwayWin: "A",
+    Draw: "D",
+};
 var manUnitedWins = 0;
 for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
     var match = matches_1[_i];
-    if (match[1] == "Man United" && match[5] == "H") {
+    if (match[1] == "Man United" && match[5] == MatchResult.HomeWin) {
         manUnitedWins++;
     }
-    else if (match[2] == "Man United" && match[5] == "A") {
+    else if (match[2] == "Man United" && match[5] == MatchResult.AwayWin) {
         manUnitedWins++;
     }
 }
