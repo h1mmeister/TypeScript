f38ed523a3180fa7e618945df88fea31afbbb6b6
h1m <himanshu.yadav643@gmail.com>
2021-05-22 17:50:12 +0530

    all about composition

---
+++ stats/build/Summary.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Summary = void 0;
+var Summary = /** @class */ (function () {
+    function Summary(analyzer, outPutTarget) {
+        this.analyzer = analyzer;
+        this.outPutTarget = outPutTarget;
+    }
+    Summary.prototype.buildAndPrintReport = function (matches) {
+        var output = this.analyzer.run(matches);
+        this.outPutTarget.print(output);
+    };
+    return Summary;
+}());
+exports.Summary = Summary;
