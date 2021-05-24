487b3d0163d0809ec5d87b7e10efb86f7ede11a5
h1m <himanshu.yadav643@gmail.com>
2021-05-24 15:24:42 +0530

    project finished

--- stats/build/Summary.js
+++ stats/build/Summary.js
@@ -1,11 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Summary = void 0;
+var HtmlReport_1 = require("./reportTargets/HtmlReport");
+var WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
 var Summary = /** @class */ (function () {
     function Summary(analyzer, outPutTarget) {
         this.analyzer = analyzer;
         this.outPutTarget = outPutTarget;
     }
+    Summary.winsAnalysisWithHtmlReport = function (team) {
+        return new Summary(new WinsAnalysis_1.WinsAnalysis(team), new HtmlReport_1.HtmlReport());
+    };
     Summary.prototype.buildAndPrintReport = function (matches) {
         var output = this.analyzer.run(matches);
         this.outPutTarget.print(output);
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,16 +1,10 @@
 import { MatchReader } from "./MatchReader";
 import { CsvFileReader } from "./CsvFileReader";
 import { Summary } from "./Summary";
-import { WinsAnalysis } from "./analyzers/WinsAnalysis";
-import { ConsoleReport } from "./reportTargets/ConsoleReport";
 
 const csvFileReader = new CsvFileReader("football.csv");
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(
-  new WinsAnalysis("Man United"),
-  new ConsoleReport()
-);
-
+const summary = Summary.winsAnalysisWithHtmlReport("Man United");
 summary.buildAndPrintReport(matchReader.matches);
